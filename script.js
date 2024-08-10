0785be9cdf9c8c4502076151e1c1ac5f57bc4637
quanhuy38 <157669413+quanhuy38@users.noreply.github.com>
2024-08-10 17:30:59 +0700

    Update script.js
    Signed-off-by: quanhuy38 <157669413+quanhuy38@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const image_holder = document.querySelectorAll(".image_holder img");
+const input = document.getElementById("input");
+
 fetch("data.json")
     .then(respone =>{
         return Response.json();
