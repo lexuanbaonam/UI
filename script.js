07f561a22c60b3486c79fdcbd3fe88cf003d5e8e
quanhuy38 <157669413+quanhuy38@users.noreply.github.com>
2024-08-10 23:10:13 +0700

    Update script.js
    Signed-off-by: quanhuy38 <157669413+quanhuy38@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-const image_holder = document.querySelectorAll(".image_holder img");
+const images_holder = document.querySelectorAll(".image_holder img");
 const input = document.getElementById("input");
+const popup = document.getElementById("pop_up");
+const popup_p = popup.querySelector("p");
 
 fetch("data.json")
-    .then(respone =>{
+    .then(Response =>{
         return Response.json();
     })
-function ShowImageData(){
-    
+
+function ShowDataImage(Clicked){
+    popup.style.display = "block";
+    popup_p.textContent = 'This is the data of image ' + Clicked.dataset.imageIndex;
+}
+
+function escape(){
+    popup.style.display = "none";
 }
